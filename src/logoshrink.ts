ad79bd194b50292f26d15fa06abc5c156110fdb9
dennismeister93 <dennis.meister93@yahoo.de>
2020-01-14 12:11:10 +0100

    Add navbar with shrinking logo and placeholder svg

---
+++ src/logoshrink.ts
@@ -0,0 +1,19 @@
+const $logo = document.querySelector('svg') as SVGSVGElement;
+
+window.onscroll = () => {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    $logo.style.width = '70';
+    $logo.style.height = '70';
+    $logo.style.marginLeft = '0';
+    $logo.style.transform = 'translate(0%, 0%)';
+  } else {
+    $logo.style.width = '300';
+    $logo.style.height = '300';
+    $logo.style.marginLeft = '50%';
+    $logo.style.transform = 'translate(-50%, 0%)';
+  }
+}
--- src/world.ts
+++
@@ -1,8 +0,0 @@
-const world = '🗺️';
-
-export function helloWorld (word: string = world): string {
-    console.log(`Hello ${world}!`);
-    return `Hello ${world}!`;
-}
-
-helloWorld();
