995c6ae3603b819e205d7685ee10a438398ba2c5
dennismeister93 <dennis.meister93@yahoo.de>
2020-01-14 15:21:50 +0100

    Change colors

--- src/logoshrink.ts
+++ src/logoshrink.ts
@@ -9,11 +9,13 @@ function scrollFunction() {
     $logo.style.width = '70';
     $logo.style.height = '70';
     $logo.style.marginLeft = '0';
+    $logo.style.fill = '#525864';
     $logo.style.transform = 'translate(0%, 0%)';
   } else {
     $logo.style.width = '300';
     $logo.style.height = '300';
     $logo.style.marginLeft = '50%';
+    $logo.style.fill = '#818C9F';
     $logo.style.transform = 'translate(-50%, 0%)';
   }
 }
